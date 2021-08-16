6b13370986d5f43c269ed29557b52d863813f9bc
AlexisBurton <86445470+AlexisBurton@users.noreply.github.com>
2021-08-15 23:55:16 -0400

    Deliverable 3

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,41 @@ SELECT DISTINCT ON (e.emp_no)e.emp_no, e.first_name, e.last_name, e.birth_date, 
 		AND (e.birth_date BETWEEN '01-01-1965' AND '12-31-1965')
 	ORDER BY e.emp_no,
 			 t.to_date DESC;
+
+-- Deliverable 3
+-- Added information: 
+  --Compare retirees to total workforce of each dept
+    SELECT e.emp_no, e.first_name, e.last_name, de.dept_no,d.dept_name
+        INTO current_employees
+        FROM employees as e
+        INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+        INNER JOIN departments as d ON (de.dept_no = d.dept_no)
+        WHERE (de.to_date = '9999-01-01');
+
+    SELECT ce.dept_no, ce.dept_name, COUNT(ce.emp_no)as total_emp, rbd.count as retiring_emp
+    	INTO loss_by_dept
+    	FROM current_employees as ce
+    	INNER JOIN ret_by_dept as rbd ON (ce.dept_no = rbd.dept_no)
+    	GROUP BY ce.dept_name,
+			 rbd.count,
+			 ce.dept_no
+    	ORDER BY ce.dept_no; 
+
+  -- Compare mentorship eligible to retiring by dept
+    SELECT ce.dept_no, ce.dept_name, count(me.emp_no) as mentorship_eligible 
+    	INTO mentorship_by_dept
+    	FROM mentorship_eligibility as me
+    	INNER JOIN current_employees as ce ON (me.emp_no = ce.emp_no)
+    	GROUP BY ce.dept_no,
+    			 ce.dept_name;
+
+    SELECT mbd.dept_no, mbd.dept_name, mbd.mentorship_eligible, rbd.count as retiring
+    	INTO mentor_v_retiring
+    	FROM mentorship_by_dept as mbd
+    	INNER JOIN ret_by_dept as rbd ON (mbd.dept_no = rbd.dept_no)
+  
+  -- Compare mentorship eligible to retiring by title
+    SELECT m.count as mentorship_eligible, r.count as retiring, r.title
+        INTO mentor_v_retiring_title
+        FROM retiring_titles as r
+        LEFT JOIN mentorship_by_title as m ON (r.title = m.title)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Pewlett-Hackard-Analysis
+# Pewlett-Hackard Silver Tsunami
+
+## Overview
+Recognizing that a good portion the workforce was nearing retirement age, Pewlett-Hackard wanted to find out what kind of liability they were facing as their employees began to retire out. 
+## Results
+### Retirement Information
+With over ninety thousand employees nearing retirement, Pewlett-Hackard certainly has its work cut out for it to maintain its workforce. Not surprisingly, many of the positions being vacated are senior positions, with Senior Engineer and Senior Staff making two-thirds of the retirees. [See retiring_titles.csv. ](Data/retiring_titles.csv) 
+### Mentorship Information
+Interestingly, while the birth years 1952-1955 yielded an average of 30k employees per year, the birth year of 1965 yields only 1,500. Despite the low numbers, the mentorship- eligible 
+## Summary
+In short, Pewlett-Hackard, needs to make
